e9d709ce3b15199b03df22148af788c0396eb0c1
MatheusINFO <matheus.add12@gmail.com>
2021-04-28 12:38:28 -0300

    test: add addDeveloper infra tests

---
+++ tests/infra/mongodb/developer-mongo-repository.spec.ts
@@ -0,0 +1,32 @@
+import { Collection } from 'mongodb'
+import { DeveloperRepository, MongoHelper } from '@/infra/mongodb'
+import { mockDeveloperParams } from '@/tests/domain/mocks'
+
+const makeSut = (): DeveloperRepository => {
+  return new DeveloperRepository()
+}
+
+describe('Developer Mongo Repository', () => {
+  let accountCollection: Collection
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.findCollection('developers')
+    await accountCollection.deleteMany({})
+  })
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('add()', () => {
+    it('Should return an developer on add success', async () => {
+      const sut = makeSut()
+      const developer = await sut.add(mockDeveloperParams())
+      expect(developer).toBeTruthy()
+    })
+  })
+})
